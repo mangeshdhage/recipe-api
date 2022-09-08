45b4b8d1e52d8c3076d9970cf257674f8b72b246
Mangesh Dhage <mangeshmdhage@gmail.com>
2022-09-08 13:44:33 +0530

    Updated security config

--- src/main/java/com/assignment/recipe/security/RecipesSecurityConfig.java
+++ src/main/java/com/assignment/recipe/security/RecipesSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.assignment.recipe.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,11 +13,14 @@ import com.assignment.recipe.util.AppConstants;
 
 @EnableWebSecurity
 public class RecipesSecurityConfig extends WebSecurityConfigurerAdapter {
+	private static final String[] SWAGGER_WHITELIST = { "/v2/api-docs", "/v3/api-docs", "/**/v3/api-docs",
+			"/swagger-resources", "/swagger-resources/**", "/configuration/ui", "/configuration/security",
+			"/swagger-ui.html", "**/swagger-ui.html", "/**/swagger-ui.html**", "/swagger-ui.html**", "/webjars/**" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/swagger-ui/**").permitAll().anyRequest().authenticated().and()
-				.httpBasic();
+		http.authorizeRequests().antMatchers(HttpMethod.POST, SWAGGER_WHITELIST).permitAll().anyRequest()
+				.authenticated().and().httpBasic().and().csrf().disable();
 	}
 
 	@Override
